44a7fafc473def8f5dedd716f2e5b69987431382
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-07-11 01:05:48 +0530

    [Sandeep] Add : check pattern matching

---
+++ Regex_Demo/Regex_Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexDemo regexDemo = new RegexDemo();
+            
+            while(true)
+            {
+                Console.WriteLine("Please choose option: ");
+                Console.WriteLine("1.Regex");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        regexDemo.MyRegex();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Regex_Demo/Regex_Demo/RegexDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Demo
+{
+    public class RegexDemo
+    {
+        static string pattern = "^cen$";
+        public void MyRegex()
+        {
+            Regex regex = new Regex(pattern);
+            if(regex.IsMatch("cen"))
+            {
+                Console.WriteLine("String is match the pattern");
+            }
+            else
+            {
+                Console.WriteLine("string does not match the pattern");
+            }
+        }
+    }
+}
